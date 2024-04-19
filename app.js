19805128ff5053923ec2c5abdf3f17862b33a621
Vikashbagra <vikashbagra611@gmail.com>
2024-04-20 01:21:41 +0530

    yes

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express =  require("express")
 
 const app = express()
 
+
+app.set("view engine", 'hbs')
+
 app.get("/",(req,res)=>{
-    res.send("hello")
+    res.render("index")
 })
+
+
+
 
 app.listen(process.env.PORT || 3000,()=>{
     console.log(`Your Server is started ${process.env.PORT || 3000}`)
