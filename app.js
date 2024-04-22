354aa592fb3ae92032cf7a1573b8704912ce8614
Vikashbagra <vikashbagra611@gmail.com>
2024-04-22 23:20:18 +0530

    yes

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express =  require("express")
 
 const app = express()
-
+const port=  process.env.PORT || 5000
 
 app.set("view engine", 'hbs')
 
 app.get("/",(req,res)=>{
-    res.send("hello")
+    res.render("index")
 })
 
 
 
 
-app.listen()
+app.listen(port,()=>{
+})
