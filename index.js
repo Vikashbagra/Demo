c6265cb2552d73c661fe1cb02f278b9a401ab7e6
Vikashbagra <vikashbagra611@gmail.com>
2024-04-22 23:25:15 +0530

    yes

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+module.exports = app;
