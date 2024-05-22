03f9e2e44342e92d517877e37101c4acc4a649dd
Vikashbagra <vikashbagra611@gmail.com>
2024-05-22 13:18:40 +0530

    yes

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+// require('dotenv').config()
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const port =  process.env.PORT
 
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index") 
 });
 
 app.listen(port,()=>{
-})
+  console.log(`Your Serve is started on Port ${port}`)
+}) 
