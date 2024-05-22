ef8770fbc2654b55b79d601e299316eea96511de
Vikashbagra <vikashbagra611@gmail.com>
2024-05-22 13:23:55 +0530

    yes the final commit

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 const express = require("express");
 const app = express();
 const port =  process.env.PORT
+const path =  require("path")
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname,"./views"))
 
 app.get("/", (req, res) => {
   res.render("index") 
